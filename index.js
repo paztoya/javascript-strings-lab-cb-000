7059a885029b15b09808700f46eaadaff76bc080
Daniel Paz <daniel@paztoya.com>
2020-01-12 01:11:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let's talk space";
 
 var conversation = `${topic}`;
